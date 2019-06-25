cc79995140b7a119e34579b961ab6d50211a82cf
Jean-André Santoni <jean.andre.santoni@gmail.com>
2019-06-25 15:35:23 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"math/big"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+
+	"goji.io"
+	"goji.io/pat"
+)
+
+func main() {
+	client, err := ethclient.Dial(os.Getenv("RPC_ENDPOINT"))
+	if err != nil {
+		panic(err)
+	}
+
+	keyJSON := os.Getenv("PRIV_KEY")
+	pass := os.Getenv("PASSPHRASE")
+	key, err := keystore.DecryptKey([]byte(keyJSON), pass)
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+
+	mux := goji.NewMux()
+	mux.Use(basicAuth)
+
+	mux.HandleFunc(pat.Get("/tx/:to"), func(w http.ResponseWriter, r *http.Request) {
+		t := common.HexToAddress(pat.Param(r, "to"))
+
+		a, errb := big.NewInt(0).SetString(pat.Param(r, "amount"), 10)
+		if errb {
+			http.Error(w, "Couldn't convert amount to big.Int", 400)
+			return
+		}
+
+		gl, err := strconv.ParseUint(pat.Param(r, "gasLimit"), 10, 64)
+		if err != nil {
+			http.Error(w, "Couldn't parse gasLimit to uint64", 400)
+			return
+		}
+
+		gp, ok := big.NewInt(0).SetString(pat.Param(r, "gasPrice"), 10)
+		if !ok {
+			http.Error(w, "Couldn't convert gasPrice to big.Int", 400)
+			return
+		}
+
+		d := []byte(pat.Param(r, "data"))
+
+		nonce, err := client.NonceAt(ctx, key.Address, nil)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+
+		tx := types.NewTransaction(nonce, t, a, gl, gp, d)
+
+		err = client.SendTransaction(ctx, tx)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+	})
+
+	http.ListenAndServe(":"+os.Getenv("PORT"), mux)
+}
