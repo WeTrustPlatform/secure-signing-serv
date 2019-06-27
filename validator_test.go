211938855ce4b41e33b29c618a91203513e064fe
Jean-André Santoni <jean.andre.santoni@gmail.com>
2019-06-27 10:48:20 +0700

    More tests

--- validator_test.go
+++ validator_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 func Test_validate(t *testing.T) {
-	t.Run("validate can run", func(t *testing.T) {
+	t.Run("can validate a transaction", func(t *testing.T) {
 		tx := types.NewTransaction(
 			1,
 			common.HexToAddress("0x5597285BbE81BaF351e2C0884e9a5f4416958862"),
@@ -27,6 +27,28 @@ end
 `
 		got, _ := validate(rules, tx)
 		want := true
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("validate = %v, want %v", got, want)
+		}
+	})
+
+	t.Run("can invalidate a transaction", func(t *testing.T) {
+		tx := types.NewTransaction(
+			1,
+			common.HexToAddress("0x5597285BbE81BaF351e2C0884e9a5f4416958861"),
+			big.NewInt(1),
+			21000,
+			big.NewInt(10000000000),
+			[]byte("abcd"),
+		)
+
+		rules := `
+function validate(tx)
+	return tx:to() == "0x5597285BbE81BaF351e2C0884e9a5f4416958862" or tx:value() == "10000000000"
+end
+`
+		got, _ := validate(rules, tx)
+		want := false
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("validate = %v, want %v", got, want)
 		}
