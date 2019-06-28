d732be880dcf3c75c0cd4a8f3f4dfc7e12ff9196
Jean-André Santoni <jean.andre.santoni@gmail.com>
2019-06-28 10:55:21 +0700

    Improve test

--- deploy_handler_test.go
+++ deploy_handler_test.go
@@ -2,22 +2,26 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"math/big"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/common"
 
 	"github.com/WeTrustPlatform/secure-signing-serv/testdata/helloworld"
 )
 
 func Test_deployHandler(t *testing.T) {
+	ctx := context.Background()
+
 	ownerKey, _ := crypto.GenerateKey()
 	owner := bind.NewKeyedTransactor(ownerKey)
 
@@ -41,11 +45,22 @@ func Test_deployHandler(t *testing.T) {
 
 		client.Commit()
 
-		fmt.Println(rr.Body)
+		if rr.Code != 200 {
+			t.Errorf("response code = %v, want %v", rr.Code, 200)
+		}
 
-		// want := big.NewInt(10000000000)
-		// if got, _ := client.BalanceAt(context.Background(), tester.From, nil); !reflect.DeepEqual(got, want) {
-		// 	t.Errorf("tester balance = %v, want %v", got, want)
-		// }
+		receipt, err := client.TransactionReceipt(ctx, common.HexToHash(rr.Body.String()))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		codeAtAddress, err := client.CodeAt(ctx, receipt.ContractAddress, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !strings.Contains(string(byteCode), string(codeAtAddress)) {
+			t.Errorf("code at address = %v, want %v", codeAtAddress, byteCode)
+		}
 	})
 }
